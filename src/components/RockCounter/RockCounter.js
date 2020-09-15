4078d7512d3abec41ca9156078b15b56678e789a
Ethan Kavanagh <ethanmkavanagh@outlook.com>
2020-09-15 15:49:42 -0500

    increase, decrease, and reset buttons working

--- src/components/RockCounter/RockCounter.js
+++ src/components/RockCounter/RockCounter.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 
 class RockCounter extends Component {
+
+  state = {
+    rockCount: 0
+  }
+
+  onIncrease = () => {
+    this.setState({
+      rockCount: this.state.rockCount + 1
+    });
+  }
+
+  onDecrease = () => {
+    this.setState({
+      rockCount: this.state.rockCount - 1
+    });
+  }
+
+  onReset = () => {
+    this.setState({
+      rockCount: 0
+    });
+  }
+//---------------------------------------------------------------------
   render() {
     return (
       <div>
           <div>
-              Rocks Picked: 0
+              Rocks Picked: {this.state.rockCount}
           </div>
           <div>
-            <button>Increase</button>
-            <button>Decrease</button>
-            <button>Reset</button>
+            <button onClick={this.onIncrease}>Increase</button>
+            <button onClick={this.onDecrease}>Decrease</button>
+            <button onClick={this.onReset}>Reset</button>
           </div>
       </div>
     );
